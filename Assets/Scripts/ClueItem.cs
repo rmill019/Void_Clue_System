76b730a5d31bb12f48542bf7aba357b832bb8a91
Ray Millares <rmill013@gmail.com>
2017-10-25 21:23:20 -0400

    Adds Raycast on Start() to retrieve basic ClueItem information

---
+++ Assets/Scripts/ClueItem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClueItem : MonoBehaviour {
+
+	private string name;
+	private int rating;
+	private string description;
+	private int xmlIndex;
+	private int pairedItemXmlIndex;
+
+	// Properties for each variable
+	public string Name
+	{
+		get { return (name); }
+
+		set { name = value; }
+	}
+
+	public int Rating 
+	{
+		get { return (rating); }
+
+		set { rating = value; }
+	}
+
+	public string Description
+	{
+		get { return (description); }
+
+		set { description = value; }
+	}
+
+	public int XMLIndex
+	{
+		get { return (xmlIndex); }
+
+		set { xmlIndex = value; }
+	}
+
+	public int PairedItemXMLIndex
+	{
+		get { return (pairedItemXmlIndex); }
+
+		set { pairedItemXmlIndex = value; }
+	}
+
+	void Awake () {
+		 
+		Name = "Lamp";
+		Rating = 4;
+		Description = "This is an ordinary Lamp";
+		XMLIndex = 1;
+		PairedItemXMLIndex = 3;
+
+		print ("Name: " + this.Name + "\n" + "Rating: " + this.Rating + "\nDescription: " + this.Description
+		+ "\n" + "XML Index: " + this.XMLIndex + "\n" + "Paired Item Index: " + this.PairedItemXMLIndex);
+
+		//print ("Camera Position: " + Camera.main.transform.position);
+	}
+		
+}
---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour {
+
+	private Camera mainCam;
+
+	void Start ()
+	{
+		mainCam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
+		Vector3 vectorToLamp = GameObject.Find ("Lamp").transform.position - mainCam.transform.position;
+		print ("Vector to Lamp: " + vectorToLamp);
+		Debug.DrawRay (mainCam.transform.position, vectorToLamp, Color.green, 10f);
+		RaycastHit objectHit;
+		if (Physics.Raycast (mainCam.transform.position, vectorToLamp, out objectHit))
+		{
+			print ("We hit: " + objectHit.collider.gameObject.name + "\n"
+				+ "Name: " + objectHit.collider.gameObject.GetComponent<ClueItem>().Name + "\n"
+				+ "Rating: " + objectHit.collider.gameObject.GetComponent<ClueItem>().Rating + "\n"
+				+ "Description: " + objectHit.collider.gameObject.GetComponent<ClueItem>().Description);
+		}
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonUp(0))
+		{
+			
+		}
+	}
+
+//	void OnMouseUpAsButton ()
+//	{
+//		
+//	}
+}
