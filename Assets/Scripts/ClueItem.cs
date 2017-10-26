e9fe305341e7e61738bf6a6485284eaa5c92f66b
Ray Millares <rmill013@gmail.com>
2017-10-25 23:03:17 -0400

    Adds basic Item rotation upon inspection

--- Assets/Scripts/ClueItem.cs
+++ Assets/Scripts/ClueItem.cs
@@ -6,9 +6,11 @@ public class ClueItem : MonoBehaviour {
 
 	public string name;
 	public int rating;
-	private string description;
+	public string description;
 	public int xmlIndex;
 	public int pairedItemXmlIndex;
+	public bool isInspectable = false;
+	public float rotateSpeed = 25f;
 
 	// Properties for each variable
 	public string Name
@@ -45,6 +47,37 @@ public class ClueItem : MonoBehaviour {
 
 		set { pairedItemXmlIndex = value; }
 	}
-		
+
+	void Update ()
+	{
+		InspectItem ();
+	}
+
+	private void InspectItem ()
+	{
+		// If the item is inspectable
+		if (isInspectable)
+		{
+			// Rotate object based on the button pressed
+			// TODO Objects not rotating as preferred.
+			if (Input.GetKey(KeyCode.W))
+			{
+				print ("Inspecting Item");
+				transform.rotation *= Quaternion.Euler (new Vector3(rotateSpeed * Time.deltaTime, 0f, 0f));
+			}
+			if (Input.GetKey (KeyCode.S))
+			{
+				transform.rotation *= Quaternion.Euler (new Vector3(-rotateSpeed * Time.deltaTime, 0f, 0f));
+			}
+			if (Input.GetKey (KeyCode.A))
+			{
+				transform.rotation *= Quaternion.Euler (new Vector3(0f, rotateSpeed * Time.deltaTime, 0f));
+			}
+			if (Input.GetKey (KeyCode.D))
+			{
+				transform.rotation *= Quaternion.Euler (new Vector3(0, -rotateSpeed * Time.deltaTime, 0f));
+			}
+		}
+	}
 		
 }
--- Assets/Scripts/RayCast.cs
+++ Assets/Scripts/RayCast.cs
@@ -7,23 +7,18 @@ public class RayCast : MonoBehaviour {
 	private Camera mainCam;
 	public float rayCastDistance = 20;
 
-	void Start ()
+	void Awake ()
 	{
 		mainCam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
-		Vector3 vectorToLamp = GameObject.Find ("Lamp").transform.position - mainCam.transform.position;
-		print ("Vector to Lamp: " + vectorToLamp);
-		Debug.DrawRay (mainCam.transform.position, vectorToLamp, Color.green, 2f);
-		RaycastHit objectHit;
-//		if (Physics.Raycast (mainCam.transform.position, vectorToLamp, out objectHit))
-//		{
-//			print ("We hit: " + objectHit.collider.gameObject.name + "\n"
-//				+ "Name: " + objectHit.collider.gameObject.GetComponent<ClueItem>().Name + "\n"
-//				+ "Rating: " + objectHit.collider.gameObject.GetComponent<ClueItem>().Rating + "\n"
-//				+ "Description: " + objectHit.collider.gameObject.GetComponent<ClueItem>().Description);
-//		}
 	}
 
 	void Update ()
+	{
+		ClickItem ();
+	}
+
+
+	void ClickItem ()
 	{
 		if (Input.GetMouseButtonUp (0))
 		{
@@ -34,13 +29,50 @@ public class RayCast : MonoBehaviour {
 			Debug.DrawRay (pos.origin, pos.direction * rayCastDistance, Color.red, 5f);
 			if (Physics.Raycast (pos.origin, pos.direction, out objectHit, rayCastDistance))
 			{
+				GameObject clueItem = objectHit.collider.gameObject;
 				print ("We hit: " + objectHit.collider.gameObject.name + "\n"
-					+ "Name: " + objectHit.collider.gameObject.GetComponent<ClueItem>().Name + "\n"
-					+ "Rating: " + objectHit.collider.gameObject.GetComponent<ClueItem>().Rating + "\n"
-					+ "Description: " + objectHit.collider.gameObject.GetComponent<ClueItem>().Description);
-			}
+				+ "Name: " + clueItem.GetComponent<ClueItem> ().Name + "\n"
+				+ "Rating: " + clueItem.GetComponent<ClueItem> ().Rating + "\n"
+				+ "Description: " + clueItem.GetComponent<ClueItem> ().Description);
 
+				// Change ClueItem isInspectable to true
+				clueItem.GetComponent<ClueItem> ().isInspectable = true;
+
+				Vector3 desiredViewingLocation = mainCam.transform.position;
+				desiredViewingLocation.x -= 1;
+				desiredViewingLocation.z += 3;
+
+				objectHit.collider.gameObject.transform.position = desiredViewingLocation;
+			}
 		}
 	}
 
 }
+
+
+//		if (Input.GetMouseButtonUp (0))
+//		{
+//			print ("Mouse Button Released");
+//			Ray pos = mainCam.ScreenPointToRay (Input.mousePosition);
+//			print ("Position: " + pos);
+//			RaycastHit objectHit;
+//			Debug.DrawRay (pos.origin, pos.direction * rayCastDistance, Color.red, 5f);
+//			if (Physics.Raycast (pos.origin, pos.direction, out objectHit, rayCastDistance))
+//			{
+//				GameObject clueItem = objectHit.collider.gameObject;
+//				print ("We hit: " + objectHit.collider.gameObject.name + "\n"
+//					+ "Name: " + clueItem.GetComponent<ClueItem>().Name + "\n"
+//					+ "Rating: " + clueItem.GetComponent<ClueItem>().Rating + "\n"
+//					+ "Description: " + clueItem.GetComponent<ClueItem>().Description);
+//
+//				// Change ClueItem isInspectable to true
+//				clueItem.GetComponent<ClueItem>().isInspectable = true;
+//
+//				Vector3 desiredViewingLocation = mainCam.transform.position;
+//				desiredViewingLocation.x -= 1;
+//				desiredViewingLocation.z += 3;
+//
+//				objectHit.collider.gameObject.transform.position = desiredViewingLocation;
+//			}
+//
+//		}
